77f45240480ef19f936788282e4ef5f7eaee70e5
inggabrielbonillar <inggabrielbonillar@gmail.com>
2022-09-05 23:27:36 -0500

    Código

---
+++ HospitalDB/appHospital/serializers/accountSerializer.py
@@ -0,0 +1,6 @@
+from appHospital.models.account import Account
+from rest_framework import serializers
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['balance', 'lastChangeDate', 'isActive']
